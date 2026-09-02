eca9006cd8cde6aa369c4ad1947b482dd3cde1bf
PilouMidas <ph.jac.beyer@gmail.com>
2026-09-02 13:33:10 +0200

    Rollback V2.2.10 service worker to V2.2.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="deauville-planning-2.2.10";
-const A=["./","./index.html","./style.css?v=2.1.0","./app.js?v=2.1.0","./v21.js?v=2.1.0","./v22.js?v=2.2.10","./data.json","./manifest.json?v=2.1.0","./icon.svg"];
+const C="deauville-planning-2.2.9";
+const A=["./","./index.html","./style.css?v=2.1.0","./app.js?v=2.1.0","./v21.js?v=2.1.0","./v22.js?v=2.2.9","./data.json","./manifest.json?v=2.1.0","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(C).then(cache=>cache.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
